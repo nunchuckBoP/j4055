b21e6d603d3953e6bc4ef843314ea61d61e10920
CJ <cj@neechsoft.com>
2018-12-07 17:49:57 -0600

    Initial commit

---
+++ I2CSensor_20181206.py
@@ -0,0 +1,155 @@
+import time
+import smbus
+import random
+class Sensor(object):
+    """
+        I2CSensor - general class for an I2CSensor
+        Author: CJ (cj@neechsoft.com)
+        Date: 11/28/2018
+        Constructor:
+            address - hex address of the i2c sensor
+            on_data - callback function when the data from the
+                      sensor is read
+
+        Methods:
+            loop_forever - loops forever continuously reading the data
+                           it also monitors the time it takes to read.
+    """
+    def __init__(self, address, sim_mode=False, on_data=None):
+         self.address = address
+         self.on_data = on_data
+         self.sim_mode = sim_mode
+         self.total_ttr = 0
+    # end of __init__
+
+    def __on_data__(self, ddict):
+        # if there is a callback function, we will
+        # call it without printing anything. if there
+        # is not a callback specified, we will print
+        # what we read.
+        if self.on_data != None:
+            self.on_data(ddict)
+        else:
+            print("data = %s, time = %s" % ())
+        # end if
+    # end __on_data__()
+
+    #removed definitions of kevin to celsius & kevin to f
+
+    def read_sim(self):
+        # this method is the else of the IF for _on_data_
+        # it simulates the reading of the sensor
+        # for testing purposes
+
+        # random time from 0.25 to 5 seconds
+        t = random.randint(250, 5000) / 1000
+
+        # value - randome number from 300 to 400
+        # kelvin
+        value = random.randint(300, 400)
+
+        # sleep for the time to simulate
+        # reading on the bus
+        time.sleep(t)
+
+        # return the random simulated
+        # value
+        return value
+    # end read_sim
+
+    def read(self):
+        
+        # get the current timestamp
+        ts1 = time.time()
+        
+        # return dictionary
+        ddict = {}
+
+        # read the actual data. This is hex data in Kelvin
+        if not self.sim_mode:
+            # initialize the bus
+            bus = smbus.SMBus(1)
+
+            # reads the data from the bus
+            raw = bus.read_word_data(self.address, 0x07)
+            raw_bin = bin(raw)
+
+            # read the time after reading data
+            ts2 = time.time() 
+
+
+            #calculation of temperatures from data read
+            kelvin = raw * 0.02
+            kelvin = round(kelvin, 2)
+            celsius = (kelvin -273.15)
+            celsius = round(celsius, 2)
+            fahrenheit = celsius * 1.8 + 32
+            fahrenheit = round(fahrenheit, 2)
+            
+            ddict['kelvin'] = kelvin       
+            ddict['celsius'] = celsius
+            ddict['fahrenheit'] = fahrenheit 
+
+            # for debugging
+            #ddict['raw_bin'] = raw_bin
+        else:
+            # read the simulated data
+            data = self.read_sim()
+        # end if
+
+        # calculate the elapse time to read to this point
+        ttr = ts2 - ts1
+        
+        # ttr is the time to read the data (removed from ddict printout)
+        # --- CJ NOTE --
+        # we need to use this outside the class, so I added back in
+        # you'll see why when you look at the on_data function in
+        # the main module
+        ddict["ttr"] = ttr
+
+        # this keeps the running ttr total
+        self.total_ttr = self.total_ttr + ttr
+        
+        # call the function
+        self.__on_data__(ddict)
+
+    # end read()
+
+    def loop_forever(self, sample_rate=0.25):
+        """
+            this method is a blocking method. This
+            should be called on the last night of the
+            main program because any code after will
+            not get executed. If a non-blocking function
+            is needed, a new thread will have to be
+            generated. Usually, I will start by inheriting
+            the thread class and daemonize it.
+        """
+        while True:
+            self.read()
+            time.sleep(sample_rate)
+        # end while
+    # end loop_forever
+
+    def take_readings(self, reading_count, sample_rate=0.25):
+
+        # reinitialize total_ttr in case this methos
+        # gets called more than once
+        self.total_ttr = 0
+        
+        for i in range(0, reading_count):
+            self.read()
+            time.sleep(sample_rate)
+        # end for
+    # end take_readings
+
+    # this method simply returns the
+    # running total
+    def get_total_ttr(self):
+        return self.total_ttr
+    # returns the total_ttr
+# end of i2c sensor_count class
+
+
+
+
---
+++ main_20181206.py
@@ -0,0 +1,140 @@
+# main.py modified for counting/timing purposes to main_count.py using I2CSensor_count.py
+# CREATION DATE: 11/28/2018
+# PROGRAM AUTHOR: CJ (CJ@NEECHSOFT.COM)
+# CPU: RASPBERRY PI 3
+# PROGRAM DESCRIPTION:
+#       THE PROGRAM TAKES READINGS FROM AN IR TEMPERATURE SENSOR
+#       USING A CUSTOM CLASS CALLED SENSOR INSIDE THE I2CSensor
+#       PYTHON FILE.
+#
+#       PROGRAM VARIABLES BELOW: SAMPLE_RATE - time the program will
+#                               wait between readings
+#                               DEVICE_ADDRESS - ADDRESS OF I2C DEVICE (hex)
+#                                                                                          READING_COUNT - NUMBER OF READINGS THE PROGRAM
+#                               WILL TAKE
+#
+#       on_data:                This is a callback function for the Sensor
+#                               class. The input variable is a dictionary
+#                               with the data inside. If there is no callback
+#                               supplied it will print the data inside of the
+#                               class method. The user can take the data outside
+#                               of the callback method for futher processing
+#
+#       device data registers:  The program reads data register 0x07 (full word)
+#       0x07                    Which takes in bytes at address 0x07 and 0x08
+#
+#       loop_forever:           This class method does the same as the take_readings
+#                               method, but the reading_count is infinite
+#
+#
+# TO RUN THE FILE:              python3 main.py or in Rpi Pixel open programming,
+#                               click on Programming>>python (2 or 3) and select
+#                               Recent Files or search folders. Rerun file: F5
+#
+#
+#       to add line numbers:    Add extension IDLEX, see: idlex.sourceforge.net
+#
+#       to format print:        see: https//www.python-course.eu/python3_formatted_output.php
+#
+#
+# import the class file. This file needs to be in the
+# same directory as this main_count.py file
+import I2CSensor_count
+
+
+
+# program variables adjustable
+# sample rate in seconds
+SAMPLE_RATE = 0.00001
+
+# device address on i2c bus
+DEVICE_ADDRESS = 0x5a
+
+#new 12/6/2018
+#added definition of addresses in sensor where data can be read...
+DATA_ADDRESS = 0x07
+
+#Known addresses in MLX90614 sensor memory are:
+#observed object temperature data address is 0x07
+
+#Ambient temperature data address is 0x06
+#Ambient temperature data can be process same as observed object temperature
+
+#Emissivity value (1.0 or less, use format %$1.3F) address is 0x04
+#Emissivity data can be read the same way but needs to be processed as a float.
+#the read temperature command is written on line 74 of I2CSensor module
+
+# number of readings you want
+# to take
+READING_COUNT = 100
+
+# on_data - callback method for the class
+# the output can be used for further processing
+# here.
+def on_data(ddict):
+    # prints out the data
+    # dictionary from the sensor.
+
+    # seeing how we don't want to print everything
+    # that comes out of the class, we will initialize a
+    # new variable called print_data. We map in the values
+    # that we want to print out.
+    print_data = {
+            'kelvin':ddict['kelvin'],
+            'celsius':ddict['celsius'],
+            'fahrenheit':ddict['fahrenheit']
+        }
+    
+    # print the data
+    print(print_data)
+
+# end on_data
+
+if __name__ == '__main__':
+
+    # instantiates the I2CSensor class. When the data is read
+    # from the sensor, the on_data method will be called.
+    sensor = I2CSensor_count.Sensor(address=DEVICE_ADDRESS, sim_mode=False, on_data=on_data)
+
+    # loop forever - blocking call
+    # use this method if you want constant readings.
+    # the input to the method is a sample rate in
+    # seconds.
+    # ---COMMENT THIS OUT IF YOU WANT A SET NUMBER OF
+    # READINGS.---
+    #sensor.loop_forever(SAMPLE_RATE)
+
+    # discrete method for a set number of readings
+    # at a given sample rate
+    # ---COMMENT THIS OUT IF YOU WANT TO CONTINUOUSLY
+
+    
+    # READ DATA.---
+    sensor.take_readings(reading_count=READING_COUNT, sample_rate=SAMPLE_RATE)
+
+    # what you had
+    #it prints this line with sum of read times
+    #print("finished reads, interval= %1.6f"% (totalttr))
+    # end main
+
+    # prints the total ttr (time to read)
+    #print("finished reads, total elasped read time in seconds = %s" % round(sensor.get_total_ttr(), 4))
+
+    #comment out the above line and uncomment the one below
+    #if you just want the total time printed
+
+    print(" Total ttr %s" % round(sensor.get_total_ttr(), 4))
+
+    #use the uncomment line below to see raw_bin from I2CSensor line 89
+    #print(bin(raw))
+
+    # how python string substitution works is: "%s" within
+    # the string, then "%" followed by the variable.
+    # for example print("this is a %s substitution" % "valid")
+    # will print out "this is a valid substitution"
+    #--------------------------------------------------
+    # if there is more than one variable substitution,
+    # then the variables have to be inside a tuple. Like this
+    # print("first %s, then %s" % ("this", "that"))
+    # that will print out "first this, then that"
+    
